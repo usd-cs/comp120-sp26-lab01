44b98fce0f1ecc1379b6bc38303d7a8d70b18490
Sat Garcia <sat@sandiego.edu>
2026-01-29 14:03:17 -0800

    add starter code for palindromes problem

---
+++ palindromes.py
@@ -0,0 +1,22 @@
+"""
+module: palindromes
+
+A module for working with palindromes.
+
+Authors:
+    - Partner A: (add your name here)
+    - Partner B: (add your name here)
+"""
+
+
+def is_palindrome(s):
+    """
+    Determines whether the given string (s) is a palindrome, where a
+    palindrome is defined as any string that is the same read forwards
+    and backwards, ignoring any spaces, punctuation, or capitalization.
+
+    Parameters:
+        s (str): The string to check if it is a palindrome.
+    """
+
+    return True # just a placeholder implementation.
